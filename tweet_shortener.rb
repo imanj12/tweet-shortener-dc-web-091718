dce61d6a667fd5c460471e7a3e63e6cd22a8fbbe
Iman Jawad <imanj12@gmail.com>
2018-09-08 18:30:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,10 +36,11 @@ def word_substituter(tweet)
 
 end
 
-def bulk_tweet_shortener
+def bulk_tweet_shortener(tweets)
   
-  
-  
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
 
 

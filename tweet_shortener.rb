cc77aa8cb7b60acf6ba5109b06e615342c89c221
Iman Jawad <imanj12@gmail.com>
2018-09-08 18:09:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -25,10 +25,10 @@ end
 def word_substituter(tweet)
   
   tweet_array = tweet.split(" ")
-  shortened_tweet_array = tweet_array.each do |obj|
+  tweet_array.each do |obj|
     shortened_word = dictionary(obj)
     if shortened_word != nil
-      obj = shortened_word
+      
     end
   end
   short_tweet = shortened_tweet_array.join(" ")
